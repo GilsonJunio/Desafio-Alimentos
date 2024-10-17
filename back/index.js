94243953e803841f962dc1cc68003d4ecf988ff3
Gilson Junio Sales Andrade <gilsonjunio@quimicojapones.com.br>
2024-10-17 14:24:10 -0300

    a

--- back/index.js
+++ back/index.js
@@ -2,34 +2,16 @@
 TODO:
 Definir função para validar valores
 */
-
 const cors = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
 const { Pool } = require('pg');
 const app = express();
-
+const port = 6000;
+const address = 'http://localhost:';
 require('dotenv').config({path:'.env.development'})
-/*console.log(process.env)
-*/
 
-const client = new Pool({
-            database: process.env.DB_NAME,
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            password: process.env.DB_PASS,
-            port: process.env.DB_PORT
-})
-
-async function buscarDados(){
-    const buscar = await client.query("SELECT * FROM alimentos")
-    const dados =  await buscar.rows
-    console.log(buscar)
-    console.log(dados)
-    return dados
-}
-buscarDados()
-/*const createPool = () => { 
+const createPool = () => { 
     if (process.env.NODE_ENV === "production") {
         return new Pool({ connectionString: process.env.DB_URL, ssl: { rejectUnauthorized: false } });
     } 
@@ -37,41 +19,41 @@ buscarDados()
         return new Pool({
             database: process.env.DB_NAME,
             user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            password: 'admin',
+            host: process.env.DB_URL,
+            password: process.env.DB_PASS,
             port: process.env.DB_PORT
         });
     }
-};*/
+};
 
-
-const port = 6000;
-const address = 'http://localhost:';
-
-function Validar_Valores(DadoRecebido,DadoNecessario){
-    const tipoDeDadoRecebido = typeof DadoRecebido
-    const tipoDeDadoNecessario = typeof DadoNecessario[1]
-    if(tipoDeDadoRecebido!=tipoDeDadoNecessario){
-        console.log('Dados incompatíveis!')
-    }
+async function buscarDados(){
+    const buscar = await pool.query("SELECT * FROM alimentos ORDER BY id")
+    const dados =  await buscar.rows
+    console.log(buscar)
+    console.log(dados)
+    return dados
 }
-Validar_Valores('olá!',0)
+
+
+
+const pool = createPool()
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/buscarAlimentos', (req, res) => {
+
     const buscar = buscarDados()
     buscar.then(dados =>{
     res.status(200).send(dados);    
     })
-//    res.send(dados)
-    
+
 });
+
 
 app.post('/enviarAlimentos', (req, res) => {
     const dadosRecebidos = req.body
-   // console.log(dadosRecebidos)
+   console.log(dadosRecebidos)
 
     const dadosParaEnviar = {
         Nome_Do_Alimento_Recebido: dadosRecebidos.dadosfornecidos.alimento,
@@ -83,12 +65,12 @@ app.post('/enviarAlimentos', (req, res) => {
         Fornecedor_Recebido: dadosRecebidos.dadosfornecidos.fornecedor,
         Tipo_De_Alimento_Recebido: dadosRecebidos.tipodealimento,
     };
-    console.log(dadosParaEnviar)
+    //console.log(dadosParaEnviar)
     if(dadosParaEnviar.AlimentoRecebido === ''|| dadosParaEnviar.TipoDeAlimentoRecebido === ''){
         res.status(400).json('Alimento vazio ou nenhum tipo de alimento especificado');        
     }
     else {
-     client.query(`
+     pool.query(`
         INSERT INTO alimentos(nome,tipo_de_alimento,data_de_validade,preco,fornecedor,url_imagem,descricao,estoque)
         values($1,$2,$3,$4,$5,$6,$7,$8)`,[
         dadosParaEnviar.Nome_Do_Alimento_Recebido,
@@ -106,12 +88,29 @@ app.post('/enviarAlimentos', (req, res) => {
 });
 
 app.put('/editarAlimentos', (req, res) => {
-    const dadosEditar = req.body;
+    const dados = req.body.dadosparaeditar;
+    
+    console.log(dados.campo_tipodealimento)
+    pool.query(
+        `UPDATE alimentos SET nome=$1, tipo_de_alimento=$2, data_de_validade=$3, preco=$4, estoque=$5, fornecedor=$6, descricao=$7, url_imagem=$8 WHERE id=$9 `
+        ,[
+            dados.campo_fruta,
+            dados.campo_tipodealimento,
+            dados.campo_datadevalidade,
+            dados.campo_preco,
+            dados.campo_estoque,
+            dados.campo_fornecedor,
+            dados.campo_descricao,
+            dados.url_imagem,
+            dados.campo_iddalinha])
+
     res.status(200).json('Alimentos editados com sucesso!');
 });
 
 app.delete('/removerAlimentos', (req, res) => {
     const dadosDeletar = req.body;
+    console.log(dadosDeletar)
+    pool.query(`DELETE FROM alimentos WHERE id=$1`,[dadosDeletar.linhaParaApagar])
     res.status(200).json('Alimentos removidos com sucesso!');
 });
 
--- script.js
+++ script.js
@@ -12,17 +12,51 @@ PARA EDITAR OU APAGAR
 ADICIONAR UM FOR NA FUNÇÃO DADOS FORNECIDOS
 
 */
+/*
+NOTAS:
+13-10-2024
+Cara, esse projetinho aqui me deu uma dorzinha de cabeça viu
+Resumidamente, a função Adicionar_Botoes_De_Apagar_e_Editar não
+estava adicionando os botões de Editar e Apagar nas células de
+ações quando eu chamava pelo corpo do código. Então eu imaginei 
+que provavelmente o DOM não carregava rápido o suficiente ao ponto 
+que função conseguisse realizar sua função apropriadamente e realmente
+era isso mesmo.
+
+Eu tentei algumas soluções como chamar a função dentro da função
+Renderizar_Dados, fiz alguns outros punctures mas infelizmente não
+funcionaram. Eu imaginei que fosse problema com o método getelements
+ByTagName. Então tentei mudar para class e usar o método querySelect
+orAll(), mas tambémnão funcionou.
+
+Então eu lembrei das Promises, as quais tecnicamente só permitem ao 
+restante do código funcionar caso ela seja cumprida, e foi isso o 
+que eu fiz. Movi o código dentro da função Renderizar_Dados responsável 
+pela renderização dos dados para dentro de uma promise e em seguida chamei
+a função Adicionar_Botoes_De_Apagar_e_Editar.
+
+Não sei dizer ao certo se a adição de async na função Renderizar_Dados
+para tratar os dados com await faz alguma diferença na promise, mas posso
+testar isso posteriormente
+
+*/
 
 const address = "http://localhost:6000"
 const servicos = {
 	enviar:"/enviarAlimentos",
 	buscar:"/buscarAlimentos",
-	editar:"/editarAlimentos"
+	editar:"/editarAlimentos",
+	remover:"/removerAlimentos"
+
 }
 
 let textarea = document.getElementsByTagName('textarea') 
 let campos = document.getElementsByTagName('input')
 let botoes = document.getElementsByTagName('button')
+let linhas = document.getElementsByTagName('tr')
+const acoes = document.getElementsByClassName('acoes')
+const td = document.getElementsByTagName('td')
+
 
 let Tipo_De_Alimento_Marcado = ()=>{
 	const fruta = campos.checkboxFruta
@@ -37,9 +71,9 @@ let Tipo_De_Alimento_Marcado = ()=>{
 		return "Usuário selecionou mais de um tipo de alimento"
 	}
 	else{
-		console.log(valoresMarcados)
+		//console.log(valoresMarcados)
 		const camposMarcados = valoresMarcados.map(campoMarcado=>{
-			console.log(campoMarcado)
+			//console.log(campoMarcado)
 			if(campoMarcado.name ==='checkboxFruta'){
 				return 'fruta'
 			}
@@ -77,8 +111,7 @@ let Dados_Fornecidos = ()=>{
 		dados.estoque,
 		dados.descricao,
 		dados.imagem,
-		dados.preco
-		]
+		dados.preco]
 	console.log(dadosParaEnviar)
 	const existemDadosVazios = dadosParaEnviar.find(dado =>{return dado === ""})
 	console.log(existemDadosVazios)
@@ -89,12 +122,6 @@ let Dados_Fornecidos = ()=>{
 		return dados		
 
 	}
-	/*
-	for(i in dados){
-		console.log(i)
-		console.log(dados.alimento)
-	}
-	*/
 }
 
 
@@ -102,105 +129,183 @@ let Dados_Fornecidos = ()=>{
 async function Buscar_Dados(){
 	const req = await fetch(address+servicos.buscar)
 	const tratar = await req.json()
-	console.log(tratar)
 	return tratar
 }
 
 function Adicionar_Botoes_De_Apagar_e_Editar(){
-	const tr = document.getElementsByTagName('tr')
-	const td = document.getElementsByTagName('td')
-	const 
-	for (let i = 1;i<tr.length; i++){
-		const checkboxSelecionar = document.createElement('input')
-		checkboxSelecionar.type='checkbox'
-		checkboxSelecionar.name=`checkbox_${i}`
-		console.log(i)
-		tr[i].appendChild(checkboxSelecionar)
-	}
-}
-function Reconhecer_Checkboxes(){
-	const checkboxes = document.getElementsByTagName('input')
-	const checkboxesEditar = []
+	const acoes = document.querySelectorAll('.acoes')
 
-	for(let i = 1; i<=checkboxes.length; i++){
-		//checkboxesEditar.push(checkboxes.namedItem(`checkbox_${i}`))
-		//console.log(checkboxes.namedItem(`checkbox_${i}`))
-		if(checkboxes.namedItem(`checkbox_${i}`)!= null){
-			checkboxesEditar.push(checkboxes.namedItem(`checkbox_${i}`))
-			//console.log(checkboxes.namedItem(`checkbox_${i}`))
-			//console.log('-----------------------------------')
-			console.log(checkboxesEditar)
-			return checkboxesEditar
+	const botoesEdicao ={
+		botaoEditar:document.createElement('button'),
+		botaoApagar:document.createElement('button')
+	}
+	botoesEdicao.botaoEditar.textContent = 'EDITAR'
+	botoesEdicao.botaoApagar.textContent = 'APAGAR'
+	
+	for(let i = 1; i<acoes.length;i++){
+		acoes[i].innerHTML=`
+			<button class="editar_${i}">EDITAR</button>
+			<button class="apagar_${i}">APAGAR</button>
+		`
+	}
+	for(let i = 0; i<botoes.length;i++){
+		let botoesEdicao = document.querySelector(`.editar_${i}`)
+		let botoesApagar = document.querySelector(`.apagar_${i}`)
+
+		if(botoesEdicao!=null || botoesApagar!=null){
+			botoesEdicao.addEventListener('click',botaoClicado=>{
+				Habilitar_Edicao(botaoClicado.target)
+			})
+			botoesApagar.addEventListener('click',botaoClicado=>{
+				const nomeDoBotaoClicado = botaoClicado.target.className
+				const ID_Do_Botao_Apertado = nomeDoBotaoClicado[nomeDoBotaoClicado.length-1]
+				Apagar_Dados(ID_Do_Botao_Apertado)
+			})		
 
 		}
 
 	}
 }
-function Reconhecer_Linhas(){
+
+function Reconhecer_Novos_Valores(ID_Da_Linha_Alterada){
+	const linhaAlterada = linhas[ID_Da_Linha_Alterada]
+	console.log(linhaAlterada)	
+	const camposDaLinhaAlterada = linhaAlterada.children
+	console.log(camposDaLinhaAlterada)
+
+	const novosValores = []
+	for(let i = 0; i<camposDaLinhaAlterada.length-1;i++){
+		const Campos_Dos_Novos_Valores = document.querySelector(`.novoscamposdados_${i}`)//camposDaLinhaAlterada[i].children
+		console.log(Campos_Dos_Novos_Valores)
+		novosValores.push(Campos_Dos_Novos_Valores.value)
+
+	}
+	console.log(novosValores)
+	return novosValores
 
 }
-function Habilitar_Edicao(){
-	const checkboxes = document.getElementsByTagName('input')
+
+function Habilitar_Edicao(Botao_Clicado){
+	let idDoBotaoClicado = Botao_Clicado.className[Botao_Clicado.className.length-1]
+
+	let LinhaParente = linhas[idDoBotaoClicado]
+	const CamposDoParente = LinhaParente.children
+	let valoresAntigos = []
+	const novosCampos = []
+	const capturarNovosValores = []
+
+	const Botao_Concluido = document.createElement('button')
+	Botao_Concluido.textContent = "CONCLUÍDO"
+	Botao_Concluido.className = `concluido_${idDoBotaoClicado}`
+
+	const Campos_De_Acoes = document.getElementsByClassName("acoes")
+
+	const Campo_Do_Botao_Clicado = Campos_De_Acoes[idDoBotaoClicado]
+	
+	const Botao_De_Editar_Atual = document.querySelector(`.${Botao_Clicado.className}`)
+
+	for(let i = 0; i< CamposDoParente.length-1;i++){
+		valoresAntigos.push(CamposDoParente[i].textContent)
+
+		const NovosCamposDeDados = document.createElement('input')
+		NovosCamposDeDados.className = `novoscamposdados_${i}`
+		NovosCamposDeDados.value = valoresAntigos[i]
+		CamposDoParente[i].replaceChildren(NovosCamposDeDados)
+	}
+	
+	Botao_De_Editar_Atual.replaceWith(Botao_Concluido)
+
+	Botao_Concluido.addEventListener('click',Botao_Clicado=>{
+		alert('DadosAtualizados!')
+		idDoBotaoClicado = Botao_Clicado.target.className.length-1
+
+		ID_Da_Linha_Alterada = Botao_Clicado.target.className[idDoBotaoClicado]
+		
+		const novosValores = Reconhecer_Novos_Valores(ID_Da_Linha_Alterada)
+		
+		for(let i = 0; i<CamposDoParente.length-1;i++){
+			CamposDoParente[i].replaceChildren(novosValores[i])
+		}
+
+		const campos = {
+			campo_iddalinha:ID_Da_Linha_Alterada,
+			campo_fruta:document.querySelector(`.fruta_${ID_Da_Linha_Alterada}`).textContent,
+			campo_tipodealimento:document.querySelector(`.tipodealimento_${ID_Da_Linha_Alterada}`).textContent,
+			campo_fornecedor:document.querySelector(`.fornecedor_${ID_Da_Linha_Alterada}`).textContent,
+			campo_datadevalidade:document.querySelector(`.datadevalidade_${ID_Da_Linha_Alterada}`).textContent,
+			campo_preco:document.querySelector(`.preco_${ID_Da_Linha_Alterada}`).textContent,
+			campo_estoque:document.querySelector(`.estoque_${ID_Da_Linha_Alterada}`).textContent,
+			campo_descricao:document.querySelector(`.descricao_${ID_Da_Linha_Alterada}`).textContent,
+			campo_imagem:document.querySelector(`.imagem_${ID_Da_Linha_Alterada}`).src
+		}
+		console.log(ID_Da_Linha_Alterada)
+		console.log(campos)
+		
+		Editar_Dados(campos)
+		const Botao_Enviar = document.createElement('button')
+	})
 }
 
-function Renderizar_Dados(){
-	const buscar = Buscar_Dados()
+
+async function Renderizar_Dados(){
 	const table = document.getElementsByTagName('table')[0]
-	buscar.then(dados =>{
-		console.log(dados)
+	const dados = await Buscar_Dados()
+	new Promise(renderizacao =>{
 		dados.map(dado=>{
-			let tr = document.createElement('tr')
-			tr.id=dado.id
-			
-			let tdFruta = document.createElement('td')
-			tdFruta.textContent = dado.nome;
-			
-			let tdDataDeValidade = document.createElement('td')			
-			tdDataDeValidade.textContent =dado.data_de_validade; 
-			
-			let tdFornecedor = document.createElement('td')			
-			tdFornecedor.textContent = dado.fornecedor;
-			
-			let tdDescricao = document.createElement('td')			
-			tdDescricao.textContent = dado.descricao;
-			
-			let tdImagem = document.createElement('td')			
-			tdImagem.textContent = dado.url_imagem;
-						
-			let tdTipoDeAlimento = document.createElement('td')			
-			tdTipoDeAlimento.textContent = dado.tipo_de_alimento;
-			
-			let tdPreco = document.createElement('td')			
-			tdPreco.textContent = dado.preco;
-			
-			let tdEstoque = document.createElement('td')
-			tdEstoque.textContent = dado.estoque;
+				let tr = document.createElement('tr')
+				tr.id=dado.id
+				
+				let tdFruta = document.createElement('td')
+				tdFruta.textContent = dado.nome;
+				tdFruta.className=`fruta_${dado.id}`
 
-			let acoes = document.createElement('td')
-			
-/*			let botaoEditar = document.createElement('button')
-			botaoEditar.textContent ="EDITAR"
-			botaoEditar.name ="editar"
-			let botaoApagar = document.createElement('button')
-			botaoApagar.textContent ="APAGAR"
-			botaoApagar.name ="apagar"
-*/
-			table.appendChild(tr)
-			tr.appendChild(tdFruta)
-			tr.appendChild(tdTipoDeAlimento)
-			tr.appendChild(tdFornecedor)
-			tr.appendChild(tdDataDeValidade)
-			tr.appendChild(tdPreco)
-			tr.appendChild(tdEstoque)
-			tr.appendChild(tdDescricao)
-			tr.appendChild(tdImagem)
-			tr.appendChild(acoes)
-/*			acoes.appendChild(botaoEditar)
-			acoes.appendChild(botaoApagar)
-*/		})
-	}) 
+				let tdDataDeValidade = document.createElement('td')			
+				tdDataDeValidade.textContent =dado.data_de_validade; 
+				tdDataDeValidade.className=`datadevalidade_${dado.id}`
+
+				let tdFornecedor = document.createElement('td')			
+				tdFornecedor.textContent = dado.fornecedor;
+				tdFornecedor.className=`fornecedor_${dado.id}`
+				
+				let tdDescricao = document.createElement('td')			
+				tdDescricao.textContent = dado.descricao;
+				tdDescricao.className=`descricao_${dado.id}`
+				
+				let tdImagem = document.createElement('img')			
+				tdImagem.src = dado.url_imagem;
+				tdImagem.className=`imagem_${dado.id}`
+							
+				let tdTipoDeAlimento = document.createElement('td')			
+				tdTipoDeAlimento.textContent = dado.tipo_de_alimento;
+				tdTipoDeAlimento.className=`tipodealimento_${dado.id}`
+				
+				let tdPreco = document.createElement('td')			
+				tdPreco.textContent = dado.preco;
+				tdPreco.className=`preco_${dado.id}`
+				
+				let tdEstoque = document.createElement('td')
+				tdEstoque.textContent = dado.estoque;
+				tdEstoque.className=`estoque_${dado.id}`
+
+				let acoes = document.createElement('td')
+				acoes.className = 'acoes'
+				table.appendChild(tr)
+				tr.appendChild(tdFruta)
+				tr.appendChild(tdTipoDeAlimento)
+				tr.appendChild(tdFornecedor)
+				tr.appendChild(tdDataDeValidade)
+				tr.appendChild(tdPreco)
+				tr.appendChild(tdEstoque)
+				tr.appendChild(tdDescricao)
+				tr.appendChild(tdImagem)
+				tr.appendChild(acoes)
+			})		
+	})
+
+	Adicionar_Botoes_De_Apagar_e_Editar()	
+
 }
-Renderizar_Dados()
+
 async function Enviar_Dados(dadosFornecidos,tipoDeAlimento){
 	if(dadosFornecidos===undefined || tipoDeAlimento===undefined){
 		return
@@ -218,7 +323,7 @@ async function Enviar_Dados(dadosFornecidos,tipoDeAlimento){
 }
 
 async function Editar_Dados(dados){
-	const req = await fetch(address+servicos.enviar,{
+	const req = await fetch('http://localhost:6000/editarAlimentos',{
 		method:'PUT',
 		headers:{'Content-Type':'Application/JSON'},
 		body:JSON.stringify({
@@ -226,30 +331,22 @@ async function Editar_Dados(dados){
 		})
 	})
 }
-async function Apagar_Dados(dados){
-	const req = await fetch(address+servicos.enviar,{
+async function Apagar_Dados(Linha_Para_Apagar){
+	const req = await fetch(address+servicos.remover,{
 		method:'DELETE',
 		headers:{'Content-Type':'Application/JSON'},
 		body:JSON.stringify({
-			dadosparaeditar:dados
+			linhaParaApagar:Linha_Para_Apagar
 		})
 	})
 }
 
-function editar(botaoClicado){
-	const parent = botaoClicado.parentElement
-	const grandparent = parent.parentElement
-	console.log(grandparent)
-/*	const tr = document.getElementsByTagName('tr')
-	const trChildren = tr.children
-	for (let i =1; i<tr.length;i++){
-		console.log(i)
-
-	}*/
-}
-
 botoes.enviar.addEventListener('click',()=>{
+	console.log(campos.datadevalidade)
+	console.log(campos.datadevalidade.value)
 	Enviar_Dados(Dados_Fornecidos(),Tipo_De_Alimento_Marcado())
 	Renderizar_Dados()
 })
 
+Renderizar_Dados() 
+Adicionar_Botoes_De_Apagar_e_Editar()
